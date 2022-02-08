0aa0e35633552b29922c8e001d1bdab77486ef5f
unknown <mestiri.am2@gmail.com>
2022-02-08 18:56:51 +0100

    corriger imports et ajouter fichiers texte

--- src/test/java/test/FirstUnitTest.java
+++ src/test/java/test/FirstUnitTest.java
@@ -2,13 +2,14 @@ package test;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
 public class FirstUnitTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 }
