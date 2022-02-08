d52808da8c513751aaa2ebabce9c824495a004b5
unknown <mestiri.am2@gmail.com>
2022-02-08 16:25:08 +0100

    First upload

---
+++ src/test/java/Test/FirstUnitTest.java
@@ -0,0 +1,34 @@
+/*package Test;
+
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class FirstUnitTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
+*/
+
+package Test;
+
+import static org.junit.Assert.*;
+
+//import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+
+
+public class FirstUnitTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
